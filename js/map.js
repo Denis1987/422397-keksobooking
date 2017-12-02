074b55817c69f677af61f25da90b76fa70325ef1
Denis1987 <denis-a1987@mail.ru>
2017-12-02 21:58:20 +0300

    был близок к победе но всё испортил))

--- js/map.js
+++ js/map.js
@@ -11,7 +11,7 @@ var title = [
   'Неуютное бунгало по колено в воде'
 ];
 
-var checkin = [
+var checkins = [
   '12:00',
   '13:00',
   '14:00'
@@ -38,18 +38,28 @@ var types = [
   'bungalo'
 ];
 
-var cards = [];
+var maxPrice = 100000;
+var minPrice = 1000;
+var maxGuests = 4;
+var minGuests = 1;
+var maxRooms = 4;
+var minRooms = 1;
+var maxTypes = 3;
+var minTypes = 0;
+var getRandomNumber = function (min, max) {
+  return Math.floor(min + Math.random() * (max - min));
+};
+var avatars = [];
+for (var k = 1; k <= 8; k++) {
+  avatars.push('img/avatars/user0' + getRandomNumber(1, 8) + '.png');
+}
 
 var pinsAmount = 8;
 
 var Template = document.querySelector('template');
 
-var pins = document.querySelector('.map__pins');
+var pinsContainer = document.querySelector('.map__pins');
 
-var coordinates = {
-  x: getRandomNumber(300, 600),
-  y: getRandomNumber(150, 350)
-}
 //это и есть наш массив
 
 var pins = [];
@@ -57,27 +67,57 @@ var pins = [];
 var showMap = document.querySelector('.map');
 showMap.classList.remove('map--faded');
 
-var getRandomNumber = function (min, max) {
-  return Math.floor(min + Math.random() * (max - min));
-};
+
+var getRandomElement = function (arr) {
+  return arr[getRandomNumber(0, arr.length)];
+}
+
+
+var getRandomElementNoRepeat = function (arr) {
+  var index = getRandomNumber(0, arr.length);
+
+  var item = arr[index];
+  arr.splice(index, 1);
+  return item;
+}
+
+
+
+var min = 0;
+
+var getRandomSlice = function (arr) {
+  var min = 0;
+  var max = arr.length;
+  var randomLeng = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  return arr.slice(min, randomLeng);
+}
+var result = getFeatures;
+
+
+
 
 
 var createCards = function () {
+  var mapCards = [];
   for (var i = 0; i < pinsAmount; i++) {
+    var coordinates = {
+      x: getRandomNumber(300, 900),
+      y: getRandomNumber(100, 500)
+    }
     var card = {
       "author": {
-        "avatar": 'img/avatars/user' + getRandomNumber(0, 8) + '.png'
+        "avatar": getRandomElementNoRepeat(avatars)
       },
 
       "offer": {
-        "title": getRandomNumber(0, 8),
+        "title": getRandomElement(title),
         "address": coordinates.x + ", " + coordinates.y,
-        "price": getRandomNumber(1000, 100000),
-        "types": getRandomNumber(0, 3),
-        "rooms": getRandomNumber(1, 4),
-        "quests": getRandomNumber(1, 4),
-        "checkins": getRandomNumber(1, 3),
-        "checkouts": getRandomNumber(1, 3),
+        "price": getRandomNumber(minPrice, maxPrice),
+        "types": getRandomNumber(minTypes, maxTypes),
+        "rooms": getRandomNumber(minRooms, maxRooms),
+        "guests": getRandomNumber(minGuests, maxGuests),
+        "checkins": getRandomElement(checkins),
+        "checkouts": getRandomElement(checkouts),
         "features": features,
         "description": '',
         "photos": []
@@ -88,53 +128,53 @@ var createCards = function () {
         "y": coordinates.y
       }
     }
-    pins.push(card);
+    mapCards.push(card);
   }
-  return cards;
+  return mapCards;
 };
-console.log(offer.card);
+
 
 //до этого места более-меннее осознано сделал а вот потом копировал и пробовал разобраться и выловить ошибки
 
 //вот я нашел в дом дереве шаблон
-var pinTemplate = document.querySelector('template').content;
+var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
 //здесь клонируется пин
 var renderPins = function (mapPin) {
   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.style.left = mapPin.coordinates.x + 'px';
-  pinElement.style.top = mapPin.coordinates.y + 'px';
+  pinElement.style.left = mapPin.location.x + 'px';
+  pinElement.style.top = mapPin.location.y + 'px';
   pinElement.querySelector('img').src = mapPin.author.avatar;
 
   return pinElement;
 };
 pins = renderPins;
 //console.log(pinTemplate);
-cards = createCards();
+var cards = createCards();
 
 
 var fragmentPins = document.createDocumentFragment();
 
 cards.forEach(function (item) {
-  fragmentPins.appendChild(renderMapPins(item));
+  fragmentPins.appendChild(renderPins(item));
 });
 
 
-pins.appendChild(fragmentPins);
+pinsContainer.appendChild(fragmentPins);
 
-var Template = commonTemplate.content.querySelector('.map__card');
+var carTemplate = Template.content.querySelector('.map__card');
 
 
 var renderCards = function (card) {
-  var cardElement = Template.cloneNode(true);
+  var cardElement = carTemplate.cloneNode(true);
 
   cardElement.querySelector('h3').textContent = card.offer.title;
   cardElement.querySelector('small').textContent = card.offer.address;
   cardElement.querySelector('.popup__price').innerHTML = card.offer.price + ' &#x20bd;/ночь';
   cardElement.querySelector('h4').textContent = card.offer.type;
   cardElement.querySelector('h4 + p').textContent = card.offer.rooms + ' комнат для ' + card.offer.guests + ' гостей';
-  cardElement.querySelector('.checkins').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  cardElement.querySelector('.checkins').textContent = 'Заезд после ' + card.offer.checkins + ', выезд до ' + card.offer.checkouts;
   cardElement.querySelector('.popup__features').innerHTML = '';
   cardElement.querySelector('.popup__features').insertAdjacentHTML('afterbegin', card.offer.features.map(getFeatures).join(' '));
   cardElement.querySelector('.popup__description').textContent = card.offer.description;
@@ -145,6 +185,6 @@ var renderCards = function (card) {
 
 var fragmentCards = document.createDocumentFragment();
 
-fragmentCards.appendChild(renderCards(cards[getRandomNumber(0, 8)]));
+fragmentCards.appendChild(renderCards(getRandomElement(cards)));
 
 showMap.appendChild(fragmentCards);
